0634c5d2eeee6d900379d056e80d3bae66731d56
apcc-chamber <apcc.chamber@gmail.com>
2025-09-06 14:16:34 -0400

    Delete components directory

--- components/DefaultSeoClient.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import { DefaultSeo } from 'next-seo';
-import { defaultSEO } from '@/lib/seo';
-
-export default function DefaultSeoClient() {
-  return <DefaultSeo {...defaultSEO} />;
-}
